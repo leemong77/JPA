5d7e9bc094d0ab0b4628eca71c7d3929db561e33
DESKTOP-2KSMOGB\window10 <spman2@daum.net>
2026-08-18 23:39:15 +0900

    first test success!!!

--- src/main/java/com/play/jpa/manage/TeamManager.java
+++ src/main/java/com/play/jpa/manage/TeamManager.java
@@ -1,6 +1,8 @@
 package com.play.jpa.manage;
 
+import com.play.jpa.entity.Team;
 import jakarta.persistence.EntityManager;
+import java.util.List;
 
 public class TeamManager {
     private final EntityManager em;
@@ -9,6 +11,21 @@ public class TeamManager {
         this.em = em;
     }
     
-    
+    public void createTeam(String teamName){
+        
+        String jpql = "select count(t) from Team t where t.name = :name";
+        Long count = em.createQuery(jpql, Long.class)
+            .setParameter("name", teamName)
+            .getSingleResult();
+        
+        if(count > 0){
+            System.out.println("duplicate teamName!!!");
+        }else{
+            Team t = new Team(); 
+            t.setName(teamName);
+
+            em.persist(t);
+        }
+    }
     
 }
--- src/test/java/com/play/jpa/manage/TeamManagerTest.java
+++ src/test/java/com/play/jpa/manage/TeamManagerTest.java
@@ -1,5 +1,6 @@
 package com.play.jpa.manage;
 
+import com.play.jpa.entity.Team;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityManagerFactory;
 import jakarta.persistence.EntityTransaction;
@@ -18,32 +19,50 @@ class TeamManagerTest {
     
     private static EntityManagerFactory emf;
     private EntityManager em;
+    private EntityTransaction tx;
     private TeamManager tm;
+    
 
     @BeforeAll
     static void setUpFactory() {
         emf = Persistence.createEntityManagerFactory("myPU");
+        
     }
 
     @AfterAll
     static void closeFactory() {
         emf.close();
+        System.out.println("------------------>아 좋다!!");
+        System.out.println("------------------>아 좋다!!");
     }
 
     @BeforeEach
     void setUp() {
         em = emf.createEntityManager();
-        //teamManager = new TeamManager(em);
+        tm = new TeamManager(em);
+        tx = em.getTransaction();
+        
+        tx.begin();
     }
 
     @AfterEach
     void tearDown() {
+        tx.commit();
         em.close();
     }
 
     @Test
-    void testAAA() {
-        System.out.println("좋다!!");
-                
+    void test_team_base() {
+        
+        String teamName = "라이온즈";
+        tm.createTeam(teamName);
+        
+        String jpql = "select t from Team t where t.name = :name";
+        List<Team> result = em.createQuery(jpql, Team.class)
+            .setParameter("name", teamName)
+            .getResultList();
+        
+        assertEquals(1, result.size());
+        
     }
 }
